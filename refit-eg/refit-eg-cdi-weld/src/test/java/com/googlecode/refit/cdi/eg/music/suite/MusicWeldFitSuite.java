813cae0372ab5886d4652efc3dc8078591c632f5
hwellmann <hwellmann@localhost>
2011-01-06 19:47:11 +0100

    added CDI example

---
+++ refit-eg/refit-eg-cdi-weld/src/test/java/com/googlecode/refit/cdi/eg/music/suite/MusicWeldFitSuite.java
@@ -0,0 +1,12 @@
+package com.googlecode.refit.cdi.eg.music.suite;
+
+import org.junit.runner.RunWith;
+
+import com.googlecode.refit.cdi.CdiFitSuite;
+import com.googlecode.refit.junit.FitConfiguration;
+
+@RunWith(CdiFitSuite.class)
+@FitConfiguration(inputDir = "src/test/fit", outputDir = "target/fit")
+public class MusicWeldFitSuite {
+
+}
