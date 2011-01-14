00e58260be32026fcdcb1739d497aba2d0e9d836
hwellmann <hwellmann@localhost>
2011-01-14 23:22:05 +0100

    added GPLv3+ copyright headers

--- refit/src/main/java/fit/Parse.java
+++ refit/src/main/java/fit/Parse.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2011 Harald Wellmann
+ * Copyright (c) 2002, 2008 Cunningham & Cunningham, Inc.
+ *
+ * This file is part of reFit.
+ * 
+ * reFit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * reFit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with reFit.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fit;
-
-// Copyright (c) 2002, 2008 Cunningham & Cunningham, Inc.
-// Released under the terms of the GNU General Public License version 2 or later.
 
 import java.io.*;
 import java.text.ParseException;
