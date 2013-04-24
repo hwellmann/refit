01fbd1c7688137527bd63488b71f18909999698a
harald wellmann <harald.wellmann@gmx.de>
2013-04-24 17:16:43 +0200

    removed hard-coded project version from test

--- refit-eg/refit-eg-osgi-test/src/test/java/com/googlecode/refit/osgi/test/MusicOsgiTest.java
+++ refit-eg/refit-eg-osgi-test/src/test/java/com/googlecode/refit/osgi/test/MusicOsgiTest.java
@@ -51,7 +51,6 @@ import fit.FixtureLoader;
 public class MusicOsgiTest {
 
     public static final String EQUINOX_HOME = "http://archive.eclipse.org/equinox/drops/R-3.6.1-201009090800/";
-    public static final String REFIT_VERSION = "1.8.0-SNAPSHOT";
 
     @Inject
     private BundleContext bundleContext;
@@ -63,30 +62,31 @@ public class MusicOsgiTest {
     public static Option[] configuration() {
 
         return options(
-            systemProperty("logback.configurationFile")
-                .value("file:src/test/resources/logback.xml"),
+                systemProperty("logback.configurationFile").value(
+                        "file:src/test/resources/logback.xml"),
 
-            // Bundles under test
-            mavenBundle("com.googlecode.refit", "refit", REFIT_VERSION),
-            mavenBundle("com.googlecode.refit", "refit-osgi", REFIT_VERSION),
-            mavenBundle("com.googlecode.refit.eg", "refit-eg-osgi-fixtures", REFIT_VERSION),
-            mavenBundle("com.googlecode.refit.eg", "refit-eg-osgi", REFIT_VERSION),
+                // Bundles under test
+                mavenBundle("com.googlecode.refit", "refit").versionAsInProject(),
+                mavenBundle("com.googlecode.refit", "refit-osgi").versionAsInProject(),
+                mavenBundle("com.googlecode.refit.eg", "refit-eg-osgi-fixtures")
+                        .versionAsInProject(),
+                mavenBundle("com.googlecode.refit.eg", "refit-eg-osgi").versionAsInProject(),
 
-            // Declarative Services (Equinox)
-            bundle(EQUINOX_HOME + "org.eclipse.equinox.ds_1.2.1.R36x_v20100803.jar").noUpdate(),
-            bundle(EQUINOX_HOME + "org.eclipse.equinox.util_1.0.200.v20100503.jar").noUpdate(),
-            bundle(EQUINOX_HOME + "org.eclipse.osgi.services_3.2.100.v20100503.jar").noUpdate(),
+                // Declarative Services (Equinox)
+                bundle(EQUINOX_HOME + "org.eclipse.equinox.ds_1.2.1.R36x_v20100803.jar").noUpdate(),
+                bundle(EQUINOX_HOME + "org.eclipse.equinox.util_1.0.200.v20100503.jar").noUpdate(),
+                bundle(EQUINOX_HOME + "org.eclipse.osgi.services_3.2.100.v20100503.jar").noUpdate(),
 
-            // Ant dependency of refit-osgi
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.ant",
-                "1.8.4_1"),
+                // Ant dependency of refit-osgi
+                mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.ant",
+                        "1.8.4_1"),
 
-            // Logging    
-            mavenBundle("org.slf4j", "slf4j-api", "1.6.4"),
-            mavenBundle("ch.qos.logback", "logback-core", "1.0.6"),
-            mavenBundle("ch.qos.logback", "logback-classic", "1.0.6"),
+                // Logging
+                mavenBundle("org.slf4j", "slf4j-api", "1.6.4"),
+                mavenBundle("ch.qos.logback", "logback-core", "1.0.6"),
+                mavenBundle("ch.qos.logback", "logback-classic", "1.0.6"),
 
-            junitBundles());
+                junitBundles());
     }
 
     @Test
@@ -95,7 +95,7 @@ public class MusicOsgiTest {
         String inputDir = getInputDir();
         System.setProperty("fit.inputDir", inputDir);
         FileRunner runner = new FileRunner(new File(inputDir), new File(getOutputDir()),
-            "MusicExample.html");
+                "MusicExample.html");
         boolean success = runner.run();
         assertTrue(success);
     }
@@ -106,7 +106,7 @@ public class MusicOsgiTest {
         String inputDir = getInputDir();
         System.setProperty("fit.inputDir", inputDir);
         TreeRunner runner = new TreeRunner(new File(inputDir), new File(getOutputDir()),
-            "MusicExample.html");
+                "MusicExample.html");
         boolean success = runner.run();
         assertTrue(success);
     }
@@ -114,14 +114,14 @@ public class MusicOsgiTest {
     private String getInputDir() {
         File refitRoot = getRoot();
         String inputDir = new File(refitRoot, "refit-eg/refit-eg-osgi-test/src/test/fit")
-            .getAbsolutePath();
+                .getAbsolutePath();
         return inputDir;
     }
 
     private String getOutputDir() {
         String refitRoot = System.getProperty("refit.root");
         String inputDir = new File(refitRoot, "refit-eg/refit-eg-osgi-test/target/fit")
-            .getAbsolutePath();
+                .getAbsolutePath();
         return inputDir;
     }
 }
